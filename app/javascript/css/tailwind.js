151f1e481f3fd961d4c17f76dab8063d0694996b
KVexcavator <str_token@mail.ru>
2020-11-16 18:14:16 +0300

    add tailwind style to new user form

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -5,7 +5,19 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: {
+          default: "#60b0e2",
+          "500": "#41a1dd",
+        }
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      }
+    },
   },
   variants: {},
   plugins: [],
