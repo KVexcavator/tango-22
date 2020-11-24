060ad83d00206704f9f8a771566dc455e88089da
KVexcavator <str_token@mail.ru>
2020-11-24 16:38:57 +0300

    add controller to Timelines

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'home#index'
+  authenticate :user do
+    resources :timelines, only: [:index, :show]
+  end
 end
